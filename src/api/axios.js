db30942768b7234285808a13f092905d68780750
OhRaekyu <dhforb1234@naver.com>
2022-06-04 22:48:59 +0900

    :recycle: API baseURL 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://13.124.248.38:8080/api',
+  baseURL: 'https://13.124.248.38:8080/api',
   headers: {
     'Content-type': 'application/json',
   },
